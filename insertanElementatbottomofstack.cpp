33d0c74575c8a9115743c792d53f3ba8936d537b
ApoorvaSunkad@15 <apoorvasunkad1999@gmail.com>
2024-02-16 15:38:56 +0530

    GFG

--- insertanElementatbottomofstack.cpp
+++ insertanElementatbottomofstack.cpp
@@ -55,3 +55,93 @@ stack<int> pushAtBottom(stack<int>& myStack, int x)
     solve(myStack,x);
     return myStack;
 }
+
+
+
+//Another modification or new approach to this problem if the return type is 
+//stack
+
+/*
+You are given a stack st of n integers and an element x. You have to insert x 
+at the bottom of the given stack. 
+
+Note: Everywhere in this problem, the bottommost element of the stack is shown 
+first while priniting the stack.
+
+Example 1:
+
+Input:
+n = 5
+x = 2
+st = {4,3,2,1,8}
+Output:
+{2,4,3,2,1,8}
+Explanation:
+After insertion of 2, the final stack will be {2,4,3,2,1,8}.
+Example 2:
+
+Input:
+n = 3
+x = 4
+st = {5,3,1}
+Output:
+{4,5,3,1}
+Explanation:
+After insertion of 4, the final stack will be {4,5,3,1}.
+Your Task:
+
+You don't need to read input or print anything. Your task is to complete the 
+function insertAtBottom() which takes a stack st and an integer x as inputs and 
+returns the modified stack after insertion.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+
+Constraints:
+1 <= n <= 105
+0 <= x, elements of stack <= 109
+
+*/
+
+
+//Using extra stack
+//T.C = O(N)
+//S.C = O(N)
+stack<int> insertAtBottom(stack<int> st,int x){
+        
+    stack<int>temp;
+    int n=st.size();
+    for(int i=0;i<n;i++){
+        temp.push(st.top());
+        st.pop();
+    }
+    st.push(x);
+    
+    for(int i=0;i<n;i++){
+        st.push(temp.top());
+        temp.pop();
+    }
+    return st;
+        
+}
+
+
+//Using recursion
+
+ stack<int> insertAtBottom(stack<int> st,int x){
+    
+    //Base case
+    if(st.empty()){
+        st.push(x);
+        return st;
+    }
+    
+    int num = st.top();
+    st.pop();
+    
+    st = insertAtBottom(st,x);
+    st.push(num);
+    
+    return st;
+        
+}
