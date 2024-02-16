39f22a6f7868d032c3c1ab3051381777cbebfa12
ApoorvaSunkad@15 <apoorvasunkad1999@gmail.com>
2024-02-16 14:50:17 +0530

    GFG

---
+++ reverseAStack.cpp
@@ -0,0 +1,74 @@
+/*
+You are given a stack St. You have to reverse the stack using recursion.
+
+Example 1:
+
+Input:
+St = {3,2,1,7,6}
+Output:
+{6,7,1,2,3}
+Explanation:
+Input stack after reversing will look like the stack in the output.
+Example 2:
+
+Input:
+St = {4,3,9,6}
+Output:
+{6,9,3,4}
+Explanation:
+Input stack after reversing will look like the stack in the output.
+Your Task:
+
+You don't need to read input or print anything. Your task is to complete the 
+function Reverse() which takes the stack St as input and reverses the given stack.
+
+Expected Time Complexity: O(N2)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= size of the stack <= 104
+-109 <= Each element of the stack <= 109
+Sum of N over all test cases doesn't exceeds 106
+Array may contain duplicate elements. 
+
+
+*/
+
+//using Recursion
+
+//T.C = O(N^2)
+//S.C = O(1)
+
+void insertAtBottom(stack<int> &S, int ele){
+    
+    //Base case
+    if(S.empty()){
+        S.push(ele);
+        return;
+    }
+    
+    int num = S.top();
+    S.pop();
+    
+    //Recursive call
+    insertAtBottom(S,ele);
+    
+    S.push(num);
+}
+public:
+void Reverse(stack<int> &St){
+    
+    //Base case
+    if(St.empty()){
+        return;
+    }
+    
+    int num = St.top();
+    St.pop();
+    
+    //Recursive call
+    Reverse(St);
+    
+    insertAtBottom(St,num);
+
+}
