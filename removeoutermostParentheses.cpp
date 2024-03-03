617b154a455440b0b3f82bd4f87702da3fd47df9
ApoorvaSunkad@15 <apoorvasunkad1999@gmail.com>
2024-03-03 20:25:09 +0530

    Leetcode

---
+++ removeoutermostParentheses.cpp
@@ -0,0 +1,78 @@
+/*
+A valid parentheses string is either empty "", "(" + A + ")", or A + B, 
+where A and B are valid parentheses strings, and + represents string concatenation.
+
+For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+A valid parentheses string s is primitive if it is nonempty, and there does not 
+exist a way to split it into s = A + B, with A and B nonempty valid parentheses 
+strings.
+
+Given a valid parentheses string s, consider its primitive decomposition: 
+s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+Return s after removing the outermost parentheses of every primitive string in 
+the primitive decomposition of s.
+
+ 
+
+Example 1:
+
+Input: s = "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: s = "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition 
+"(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, 
+this is "()()" + "()" + "()(())" = "()()()()(())".
+
+Example 3:
+
+Input: s = "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either '(' or ')'.
+s is a valid parentheses string.
+
+
+*/
+
+//T.C = O(N)
+//S.C = O(N)
+
+string removeOuterParentheses(string s) {
+    stack<char> st;
+
+    string ans = "";
+
+    for(int i = 0; i<s.length();i++){
+        char ch = s[i];
+
+        if(ch=='('){
+            if(st.size()>0){
+                ans+=ch;
+            }
+            st.push(ch);
+        }
+        else{
+            st.pop();
+            if(st.size()>0){
+                ans+=ch;
+            }
+        }
+    }
+    return ans;
+}
